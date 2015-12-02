40126593f15c78e8d29375ac1514718fbc0ea2c1
Sam Corcos <sam@corcos.io>
2015-12-02 09:24:21 -0800

    adding phoenix boilerplate

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,10 @@
+defmodule HelloPhoenix.HelloController do
+  use HelloPhoenix.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, "show.html", messenger: messenger
+  end
+end
